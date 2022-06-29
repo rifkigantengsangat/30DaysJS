c27e0a9b6e39a0b4b162cb8fd48a5ac626045f13
rifkigantengsangat <radenmuhamadrifki@gmail.com>
2022-06-29 21:23:42 +0700

    buat kode generator lanjut bikin scroll

--- index.js
+++ index.js
@@ -77,9 +77,70 @@ const namasaya =(callback)=>{
 }
 namasaya(greeting)
 
-// promise
+promise
 const promise = new Promise ((resolve, reject) => {
   setTimeout(()=>{ 
     resolve("halo")
   },3000)
 }).then(value =>console.log(value))
+const submit = document.querySelector("#submit")
+function Ada(ada){
+   submit.addEventListener("click",()=>{
+
+    const tampung = document.querySelector('.tampung')
+   const p = document.createElement("p")
+   p.innerHTML = `token anda adalah ${ada}`
+   tampung.appendChild(p)
+ setTimeout(() => { 
+    window.location.reload()
+  }, 3000)
+   })
+}
+
+function generateCode(batas) {
+    const arr =[]
+    const data =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',1,2,3,4,5,6,7,8,9,10]
+ 
+
+   for(let i = 0; i<batas; i++){
+        const random = Math.floor(Math.random() *data.length)    
+        arr.push(data[random])         
+     arr.map((el,i,array)=>{
+        if(array.indexOf(el)!== array.lastIndexOf(el)){
+            array.splice(i,1)
+        }
+       })
+      
+    const joining = arr.join('')
+       sayHelloRandom(joining)
+    
+   
+   }
+   return arr
+}
+const random = generateCode(5)
+
+function sayHelloRandom (nama){
+    const Data = ['a','i','u','e','o']
+    const arr =[]
+    const random = Math.floor(Math.random()*Data.length)
+    arr.push(Data[random])
+   if(nama.length <= 6){
+    const gabung = [...nama,...arr].join('')
+   Ada(gabung)
+   
+   
+   }
+   return arr
+  
+}
+
+window.addEventListener("scroll",()=>{
+    const height = window.scrollY
+   const buletNumber = Math.floor(height)
+    if(buletNumber >=60){
+      const createElement = document.createElement("div")
+      const giftkelas = createElement.classList.add("giftkelas")
+      
+    }
+})
